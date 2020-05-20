514cbc60ccbdf0321e70d584c0d8d3c13686ed30
Daniel de Sales <ssales.daniel@gmail.com>
2020-05-20 09:51:11 -0300

    adicionei chamada api

--- screens/HomeView/HomeView.js
+++ screens/HomeView/HomeView.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "react-native";
+import axios from "axios";
 import styled from "styled-components";
 
 import { colors } from "../../styles";
@@ -8,6 +9,26 @@ import About from "./components/About";
 import Forecast from "./components/Forecast";
 
 export default function HomeView() {
+  const [weatherInfo, setWeatherInfo] = useState([]);
+  const apiUrl =
+    "http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/3477/days/15?token=bda1bab21ddc54b9546de7a72a53fc46";
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  const getWeather = () => {
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        console.log(response.data.data[0]);
+        setWeatherInfo(response.data.data[0]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <Container>
       <StatusBar barStyle="light-content" />
